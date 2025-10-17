fa837f95ef5aa5226e9a6028ab8a461c4bafc189
Quang Nam <wgnam@MacMac.local>
2025-10-17 09:05:13 +0700

    code #33

---
+++ src/components/Todo/TodoData.jsx
@@ -0,0 +1,12 @@
+const TodoData = () => {
+    return (
+        <div>
+            <div className='todo-data'>
+                <div>Learning react</div>
+                <div>watching youtube</div>
+            </div>
+        </div>
+    )
+}
+
+export default TodoData
