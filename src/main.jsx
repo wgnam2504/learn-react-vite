c0612bfd30708a265c02b3601cd29424da1b6952
Quang Nam <wgnam@MacMac.local>
2025-10-22 09:40:46 +0700

    code #46

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import UserPage from './pages/user.jsx';
+import ProductPage from './pages/product.jsx';
+import './styles/global.css'
 
 const router = createBrowserRouter([
   {
@@ -13,19 +18,19 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>Login page</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>Register page</div>,
+    element: <RegisterPage />,
   },
   {
     path: "/users",
-    element: <div>Users page</div>,
+    element: <UserPage />,
   },
   {
     path: "/products",
-    element: <div>Products page</div>,
+    element: <ProductPage />,
   },
 ]);
 
